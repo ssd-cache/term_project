7e2cad40f6225fd35e318a71683e5a6a6596c488
ssd-cache <124332301@qq.com>
2014-06-07 11:26:26 -0700

    update

--- cache_ssd/cache_ssd.h
+++ cache_ssd/cache_ssd.h
@@ -1,17 +1,18 @@
+//#define TRACE_DIR ".//trace//"
 struct output_entry
 {
 	float time_stamp;
 	int page_fault;
 	float hit_rate;
 };
-
-struct SizeInfo
-{
-	char* type;
+struct trace {
+	char* addr;
 	int size;
+	char* mode;
+	float time_stamp;
 };
 
+void trace_parser(char *trace_file, char *result);
 void output_helper(struct output_entry input, char *output_file);
-void simulation(char *test_file, int cnt_trace, SizeInfo *sizeArr);
+void simulation(char *test_file, char *file_name);
 void result_parser(char *result_file);
-void getSizeInput(SizeInfo *sizeArr);
