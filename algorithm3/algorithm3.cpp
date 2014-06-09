7feca15b12921a152ba814bd7ff86818ae685dc5
Elric86 <noooooobody@hotmail.com>
2014-06-09 07:50:28 -0700

    Adding algorithm3

--- algorithm3/algorithm3.cpp
+++ algorithm3/algorithm3.cpp
@@ -4,14 +4,235 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-
+#include <iostream>
+#include <vector>
 #include "algorithm3.h"
+#include "cache_ssd.h"
 
-void algorithm3()
+using namespace std;
+
+struct page {
+	int coldFlag;
+	int id;
+	bool clean;
+};
+
+//vector<page> cache;
+vector<page> L1;
+vector<page> L2;
+vector<page> L3;
+const int BOUNDARY = 4;
+const int COOLMAX = 10;
+int count;
+int page_fault = 0;
+const int max_pages = SIZE_OF_MEMORY / CACHE_LINE_SIZE;
+int num_pages = max_pages/2;
+
+void adjust_window(char* mode)
 {
-
-	printf("this is the 3th algorithm\n");
-
+	if(mode == "R" && L3.at(L3.size()-1).clean && L3.at(L3.size()-2).clean)
+	{
+		num_pages = num_pages*0.8;
+		if(L1.size() > num_pages)
+		{
+			vector<page> tempL1;
+			for(int i = L1.size()-num_pages; i < L1.size(); i++)
+			{
+				tempL1.push_back(L1.at(i));
+			}
+			L1 = tempL1;
+		}
+		if(L2.size() > num_pages)
+		{
+			vector<page> tempL2;
+			for(int i = L2.size()-num_pages; i < L2.size(); i++)
+			{
+				tempL2.push_back(L2.at(i));
+			}
+			L2 = tempL2;
+		}
+	}
+	else if(mode == "W" && !L3.at(L3.size()-1).clean && !L3.at(L3.size()-2).clean && num_pages*1.2 < max_pages)
+		num_pages = num_pages*1.2;
 }
 
+void replace(int targetList, int targetIndex, page target)
+{
+	page replacePage;
+	if(targetList == 1)
+	{
+		replacePage = L1.at(targetIndex);
+		L1.at(targetIndex) = target;
+	}
+	else
+	{
+		replacePage = L2.at(targetIndex);
+		L2.at(targetIndex) = target;
+	}
+	L3.push_back(replacePage);
+}
+
+void findPageToReplace(page target, char* mode)
+{
+	if(!L2.empty())
+		replace(2,0, target);
+	else
+	{
+		adjust_window(mode);
+		bool found = false;
+		page tempPage;
+		for(int i = 0; i < L1.size(); i++)
+		{
+			tempPage = L1.at(i);
+			if(tempPage.clean)
+			{
+				replace(1,i,target);
+				found = true;
+				break;
+			}
+			else
+				tempPage.coldFlag ++;
+		}
+
+		if(!found)
+		{
+			for(int i = 0; i < L1.size(); i++)
+			{
+				tempPage = L1.at(i);
+				if(tempPage.coldFlag >= BOUNDARY)
+				{
+					replace(1,i,target);
+					found = true;
+					break;
+				}
+			}
+		}
+
+		if(!found)
+			replace(1,0,target);
+	}
+}
+
+void agePages()
+{
+	for(int i = 0; i < L1.size(); i++)
+	{
+		page tempPage = L1.at(i);
+		if(tempPage.coldFlag < COOLMAX)
+		{
+			tempPage.coldFlag++;
+			if(tempPage.clean && tempPage.coldFlag >= BOUNDARY)
+			{
+				L2.push_back(tempPage);
+				L1.erase(L1.begin()+i);			
+			}
+		}
+	}
+	for(int k = 0; k < L2.size(); k++)
+	{
+		page tempPage = L2.at(k);
+		if(tempPage.coldFlag < COOLMAX)
+			tempPage.coldFlag++;
+	}
+}
+
+struct output_entry aml(int pid, char* mode)
+{
+	output_entry stat;
+	page newPage;
+	newPage.coldFlag = 0;
+	newPage.id = pid;
+	bool found = false;
+
+	if(mode == "R")
+		newPage.clean = true;
+	else
+		newPage.clean = false;
+	
+	//Look for page hit
+	for(int i = 0; i < L1.size(); i++)
+	{
+		page tempPage = L1.at(i);
+		if (tempPage.id == pid)
+		{
+			tempPage.coldFlag = 0;
+			L1.push_back(tempPage);
+			L1.erase(L1.begin()+i);
+			agePages();
+			found = true;
+		}
+	}
+
+	if(!found)
+	{
+		//if(cache.size() < max_pages)
+		if(L1.size() < num_pages)
+		{
+			//cache.push_back(newPage);
+			L1.push_back(newPage);
+		}
+		else
+			findPageToReplace(newPage,mode);
+	
+		agePages();
+		page_fault++;
+	}
+
+	return stat;
+}
+
+void trace_parse(char *trace_file, char* result)
+{
+	FILE * trace_f;
+	int i;
+	int page_idx;
+	char *p;
+	char *array[4];
+	count = 0;
+	trace sample;
+	output_entry output;
+	trace_f = fopen(trace_file, "r");
+	if (trace_f != NULL)
+	{
+		char line[128];
+		while (fgets(line, sizeof(line), trace_f) != NULL)
+		{
+			i = 0;
+
+			//fputs(line, stdout);
+			//split the line into the struct
+			p = strtok(line, ",");
+			while (p != NULL)
+			{
+				array[i++] = p;
+				p = strtok(NULL, ",");
+			}
+			sample.addr = array[0];
+			sample.size = atoi(array[1]);
+			sample.mode = array[2];
+			sample.time_stamp = atof(array[3]);
+			//page_ref[cnt_transfer] = atoi(sample.addr);
+			long value = strtol(sample.addr, NULL, 16);
+			page_idx = value / CACHE_LINE_SIZE;
+			//page_idx = atoi(sample.addr) / CACHE_LINE_SIZE;
+			output.time_stamp = sample.time_stamp;
+			//call the algorithm
+			//output_entry temp = larc(page_idx);
+			//output.page_fault = temp.page_fault;
+			//output.hit_rate = temp.hit_rate;
+			//output_helper(output, result);
+			output_entry temp = aml(page_idx, sample.mode);
+			output.page_fault = temp.page_fault;
+			output.hit_rate = temp.hit_rate;
+			output_helper(output, result);
+		}
+		fclose(trace_f);
+	}
+}
+
+void algorithm3(char *sim_file, char *output)
+{
+	printf("this is the 3rd algorithm\n");
+	trace_parse(sim_file, output);
+    printf("output the result to %s\n", output);
+}
--- algorithm3/algorithm3.h
+++ algorithm3/algorithm3.h
@@ -1,1 +1,11 @@
+//4M
+#define CACHE_SIZE 4194304
+//1M
+#define CACHE_LINE_SIZE 409600
+#define CACHE_ACCESS_SPEED 2
+#define CACHE_REPLACE_SPEED 1
+#define HDD_ACCESS_SPEED 20
+//1G
+#define SIZE_OF_MEMORY 1073741824
+
 void algorithm3();
